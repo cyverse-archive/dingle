5721a4d119830ef7518b4454ed3147b980e277cf
John Wregglesworth <wregglej@iplantcollaborative.org>
2012-07-31 09:04:17 -0700

    Added the (combine) (chain) and (checked) macros.
    They simply call out to stevedore's (do-script) (chained-script) and (checked-script), respectively.

--- src/dingle/scripting.clj
+++ src/dingle/scripting.clj
@@ -14,6 +14,21 @@
   `(with-script-language :pallet.stevedore.bash/bash
     (script ~@body)))
 
+(defmacro combine
+  [& scripts]
+  `(with-script-language :pallet.stevedore.bash/bash
+     (do-script ~@scripts)))
+
+(defmacro chain
+  [& scripts]
+  `(with-script-language :pallet.stevedore.bash/bash
+     (chained-script ~@scripts)))
+
+(defmacro checked
+  [& scripts]
+  `(with-script-language :pallet.stevedore.bash/bash
+     (checked-script ~@scripts)))
+
 (defn sudo-cmd
   [cmd pass]
   (str "echo " pass " | sudo -S " cmd))
