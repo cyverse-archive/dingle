26e1fa6161780c5b4dd03411dd0d63a50ce67fbf
Dennis Roberts <dennis@iplantcollaborative.org>
2013-01-30 16:28:34 -0700

    CORE-3929: switched from clojure.data.json to cheshire

--- project.clj
+++ project.clj
@@ -5,11 +5,12 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.clojure/tools.cli "0.2.1"]
-                 [org.iplantc/clojure-commons "1.3.0-SNAPSHOT"]
+                 [org.iplantc/clojure-commons "1.4.0-SNAPSHOT"]
                  [org.cloudhoist/stevedore "0.7.2"]
                  [clj-ssh "0.4.0"]
                  [slingshot "0.10.1"]
                  [com.cemerick/url "0.0.6"]
+                 [cheshire "5.0.1"]
                  [org.eclipse.jgit/org.eclipse.jgit "2.0.0.201206130900-r"]]
   :repositories {"sonatype"
                  "http://oss.sonatype.org/content/repositories/releases"
